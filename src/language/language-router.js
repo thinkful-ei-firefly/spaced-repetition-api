68a57bea3c2ea0875576bfff83fac32ef39dfa1a
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-10-17 16:24:41 -0400

    remove console logs and all tests passing

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -119,8 +119,6 @@ languageRouter.post('/guess', jsonParser, async (req, res, next) => {
       isCorrect: guess.toLowerCase() === words[0].translation.toLowerCase()
     };
 
-    console.log(response);
-    
     return res.status(200).json(response);
   } catch (error) {
     next(error);
